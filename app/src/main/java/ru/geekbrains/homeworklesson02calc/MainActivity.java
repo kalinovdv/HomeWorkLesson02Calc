7868c74fedb5b144e925075b284f8d704789a1ed
kalinovdv <kdv_ul@rambler.ru>
2021-06-25 12:15:33 +0400

    Добавил обработку нажатий на кнопки.
    В процедуру onClick добавил код для обработки нажатий на кнопки.
    Перенес поля класса тип Button в локальные переменные процедуры initButtons. Изменил привязку элемента TextView.

--- app/src/main/java/ru/geekbrains/homeworklesson02calc/MainActivity.java
+++ app/src/main/java/ru/geekbrains/homeworklesson02calc/MainActivity.java
@@ -1,9 +1,8 @@
 package ru.geekbrains.homeworklesson02calc;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,23 +11,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView textView;
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private Button button5;
-    private Button button6;
-    private Button button7;
-    private Button button8;
-    private Button button9;
-    private Button button0;
-    private Button buttonAddition;
-    private Button buttonSubtraction;
-    private Button buttonMultiplication;
-    private Button buttonDivision;
-    private Button buttonEqually;
-    private Button buttonDot;
-    private Button buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,60 +26,113 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initButtons(){
-        button0 = findViewById(R.id.button0);
+        Button button0 = findViewById(R.id.button0);
         button0.setOnClickListener(this);
 
-        button1 = findViewById(R.id.button1);
+        Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(this);
 
-        button2 = findViewById(R.id.button2);
+        Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(this);
 
-        button3 = findViewById(R.id.button3);
+        Button button3 = findViewById(R.id.button3);
         button3.setOnClickListener(this);
 
-        button4 = findViewById(R.id.button4);
+        Button button4 = findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
-        button5 = findViewById(R.id.button5);
+        Button button5 = findViewById(R.id.button5);
         button5.setOnClickListener(this);
 
-        button6 = findViewById(R.id.button6);
+        Button button6 = findViewById(R.id.button6);
         button6.setOnClickListener(this);
 
-        button7 = findViewById(R.id.button7);
+        Button button7 = findViewById(R.id.button7);
         button7.setOnClickListener(this);
 
-        button8 = findViewById(R.id.button8);
+        Button button8 = findViewById(R.id.button8);
         button8.setOnClickListener(this);
 
-        button9 = findViewById(R.id.button9);
+        Button button9 = findViewById(R.id.button9);
         button9.setOnClickListener(this);
 
-        buttonAddition = findViewById(R.id.buttonAddition);
+        Button buttonAddition = findViewById(R.id.buttonAddition);
         buttonAddition.setOnClickListener(this);
 
-        buttonSubtraction = findViewById(R.id.buttonSubtraction);
+        Button buttonSubtraction = findViewById(R.id.buttonSubtraction);
         buttonSubtraction.setOnClickListener(this);
 
-        buttonMultiplication = findViewById(R.id.buttonМultiplication);
+        Button buttonMultiplication = findViewById(R.id.buttonМultiplication);
         buttonMultiplication.setOnClickListener(this);
 
-        buttonDivision = findViewById(R.id.buttonDivision);
+        Button buttonDivision = findViewById(R.id.buttonDivision);
         buttonDivision.setOnClickListener(this);
 
-        buttonEqually = findViewById(R.id.buttonEqually);
+        Button buttonEqually = findViewById(R.id.buttonEqually);
         buttonEqually.setOnClickListener(this);
 
-        buttonDot = findViewById(R.id.buttonDot);
+        Button buttonDot = findViewById(R.id.buttonDot);
         buttonDot.setOnClickListener(this);
 
-        buttonClear = findViewById(R.id.buttonClear);
+        Button buttonClear = findViewById(R.id.buttonClear);
         buttonClear.setOnClickListener(this);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()){
+            case R.id.button0:
+                textView.setText("Нажата кнопка 0");
+                break;
+            case R.id.button1:
+                textView.setText("Нажата кнопка 1");
+                break;
+            case R.id.button2:
+                textView.setText("Нажата кнопка 2");
+                break;
+            case R.id.button3:
+                textView.setText("Нажата кнопка 3");
+                break;
+            case R.id.button4:
+                textView.setText("Нажата кнопка 4");
+                break;
+            case R.id.button5:
+                textView.setText("Нажата кнопка 5");
+                break;
+            case R.id.button6:
+                textView.setText("Нажата кнопка 6");
+                break;
+            case R.id.button7:
+                textView.setText("Нажата кнопка 7");
+                break;
+            case R.id.button8:
+                textView.setText("Нажата кнопка 8");
+                break;
+            case R.id.button9:
+                textView.setText("Нажата кнопка 9");
+                break;
+            case R.id.buttonAddition:
+                textView.setText("Нажата кнопка +");
+                break;
+            case R.id.buttonSubtraction:
+                textView.setText("Нажата кнопка -");
+                break;
+            case R.id.buttonМultiplication:
+                textView.setText("Нажата кнопка *");
+                break;
+            case R.id.buttonDivision:
+                textView.setText("Нажата кнопка /");
+                break;
+            case R.id.buttonEqually:
+                textView.setText("Нажата кнопка =");
+                break;
+            case R.id.buttonDot:
+                textView.setText("Нажата кнопка .");
+                break;
+            case R.id.buttonClear:
+                textView.setText("Нажата кнопка C");
+                break;
+        }
     }
 }
